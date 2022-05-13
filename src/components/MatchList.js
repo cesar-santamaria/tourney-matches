f9b7038c3087a84696561c4c0c0ab245fb565561
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-13 16:39:33 -0500

    add components

---
+++ src/components/MatchList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MatchList() {
+  return (
+  <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {/* Matches will be shown here */}
+  </section>
+  )
+}
---
+++ src/components/PlayerList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PlayerList() {
+  return (
+  <section className="PlayerList">
+    <h1>Current participating players</h1>
+    {/* Players will be shown here */}
+  </section>
+  )
+}
